54c6cc4dc5c377eb90753023a9c74f3a959420e0
Antoine Sachet <antoine.sac@gmail.com>
2016-11-30 10:43:48 +0000

    New convenience function rotate_axis_text for ggplot2 and two custom continuous color schemes scale_color/fill_extreme.

--- R/plotting.R
+++ R/plotting.R
@@ -10,5 +10,68 @@
 ##'  mtcars %>% plot_hist("mpg")
 ##' @export
 plot_hist <- function(df, var, main=paste("Histogram of", var), xlab=var, ...) {
-  hist(df[[var]], main=main, xlab=xlab)
+  hist(df[[var]], main=main, xlab=xlab, ...)
+}
+
+
+#' Custom color scales for ggplot2
+#'
+#' @description A collection of color/fill scales for ggplot2.
+#' @name color_scales
+NULL
+
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom ggplot2 scale_color_gradientn scale_fill_gradientn
+#' @describeIn color_scales  A 'color' scale highlighting extreme values.
+#' @export
+scale_color_extreme <- function(...){
+  scale_color_gradientn(colours=c(brewer.pal(n=9, "RdYlBu")[-(5:8)], "turquoise2"), ...)
+}
+
+#' @describeIn color_scales  A 'fill' scale highlighting extreme values.
+#' @importFrom RColorBrewer brewer.pal
+#' @export
+scale_fill_extreme <- function(...){
+  scale_fill_gradientn(colours=c(brewer.pal(n=9, "RdYlBu")[-(5:8)], "turquoise2"), ...)
+}
+
+#' Rotate axis text of a ggplot
+#'
+#' Adapted from http://stackoverflow.com/a/36979201/3498910.
+#'
+#' @param angle Angle to apply, in degrees
+#' @param position One of "x" or "y". Defaults to "x".
+#' @param hjust,vjust Horizontal and vertical adjustement.
+#'
+#' @importFrom ggplot2 element_text theme
+#' @examples
+#' library("ggplot2")
+#' df <- data.frame(x=.5, y=.5)
+#'
+#' ggplot(df, aes(x=x, y=y)) + geom_point() +
+#'     rotate_axis_text(60)
+#' @export
+rotate_axis_text <- function(angle, position="x", hjust="auto", vjust="auto") {
+
+  angle     = angle[1];
+  position  = position[1]
+  positions = list(x=0,y=90)
+  if(!position %in% names(positions))
+    stop(sprintf("'position' must be one of [%s]",paste(names(positions),collapse=", ")))
+  if(!is.numeric(angle))
+    stop("'angle' must be numeric")
+  rads  = (-angle - positions[[ position ]])*pi/180
+  if(hjust=="auto")
+    hjust = 0.5*(1 - sin(rads))
+  if(vjust=="auto")
+    vjust = 0.5*(1 + cos(rads))
+
+  if(position=="x") {
+    theme(axis.text.x = element_text(angle=angle,
+                                     hjust=hjust, vjust=vjust))
+  } else{
+    theme(axis.text.y = element_text(angle=angle,
+                                     hjust=hjust, vjust=vjust))
+  }
+
 }
